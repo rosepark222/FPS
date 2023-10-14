8b58ac67e85ea4fecc476d0e6f180974168cd38f
ryoungsun park <rosepark222@gmail.com>
2023-10-14 11:34:21 -0700

    success in sync ball location ; used fixedUpdate, lerp interpolation with lerpSpeed=0.5, in the ball object, separated server work (update ball location) and client work (rb.Move ), learn the camera.main.transform.forward is the vector pointing to the view of char

--- Assets/FishNet/zScripts/ObjToSpawnScript.cs
+++ Assets/FishNet/zScripts/ObjToSpawnScript.cs
@@ -2,44 +2,129 @@ using System.Collections;
 using System.Collections.Generic;
 using FishNet.Object;
 using UnityEngine;
+using FishNet.Object.Synchronizing;
 
 public class ObjToSpawnScript : NetworkBehaviour
 {
+    [SerializeField] GameObject objToSpawn;
     Rigidbody rb;
     CharacterController characterController;
-    Vector3 nextPosition; // = Vector3.zero;
-    Vector3 moveDirection = Vector3.zero;
+    [SyncVar] public Vector3 nextPosition; // = Vector3.zero; // new Vector3(434.0f, 218.5f, -3.4f);
+    [SerializeField]  public Vector3 initialPosition;
+    [SerializeField]  public Vector3 moveDirection; // = Vector3.zero;
     [SerializeField]   Vector3 forward;
-    [SerializeField] float bulletSpeed = 5f;
+    [SerializeField] float bulletSpeed = 30f;
+ 
+
+    private Camera playerCameraForward;
+
+
+    private void Awake()
+    {
+        nextPosition = initialPosition;
+        Debug.Log(string.Format("Awake in ObjToSpawnScript nextPosition = initialPosition; {0} {1} {2}", nextPosition.x, nextPosition.y, nextPosition.z));
+
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("Start in ObjToSpawnScript");
+        Debug.Log("Start in ObjToSpawnScript before checking ownership");
+        if (!base.IsOwner) return;
+        Debug.Log("Start in ObjToSpawnScript after checking ownership");
         //Vector3 forward = transform.TransformDirection(Vector3.forward);
         //nextPosition = (forward * bulletSpeed);
         //transform.position += nextPosition;
-        //characterController = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
         rb = GetComponent<Rigidbody>();
         nextPosition = transform.position;
+        Debug.Log(string.Format("Start in ObjToSpawnScript nextPosition {0} {1} {2}", nextPosition.x, nextPosition.y, nextPosition.z));
+
+        // in server, this velicity makes the ball to travel in line
+        // in different client, the position of the ball was not in sync -- makes the ball to drop and move slowly
+        // rb.velocity = Camera.main.transform.forward * bulletSpeed;
+        //moveDirection = Camera.main.transform.forward;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
+       // Debug.Log(string.Format("Update in ObjToSpawnScript  "));
 
-        forward = transform.TransformDirection(Vector3.forward);
-        //Debug.Log(string.Format("Update in ObjToSpawnScript {0} {1} {2}", forward.x, forward.y, forward.z));
+        if (base.IsServer)
+        {
+            //forward = transform.TransformDirection(Vector3.forward);
 
-        //nextPosition += (forward * bulletSpeed);
-        //transform.position += nextPosition;
-        //characterController.Move(nextPosition * Time.deltaTime);
-        //rb.Move(nextPosition * Time.deltaTime, Quaternion.identity);
-        //rb.Move(transform.position, Quaternion.identity);
+            //nextPosition += (forward * bulletSpeed);
+            //transform.position += nextPosition;
+            //characterController.Move(nextPosition * Time.deltaTime);
+            //rb.Move(nextPosition * Time.deltaTime, Quaternion.identity);
+            //rb.Move(transform.position, Quaternion.identity);
+            //moveDirection = forward;
 
+            nextPosition = nextPosition + moveDirection * bulletSpeed * Time.deltaTime;
+            Debug.Log(string.Format("Update in ObjToSpawnScript Server nextPosition {0} {1} {2}", nextPosition.x, nextPosition.y, nextPosition.z));
 
-        nextPosition += Vector3.forward * bulletSpeed * Time.deltaTime;
+        }
+
+        //if (!base.IsOwner) return;
+        if (base.IsClient)
+        {
+            float lerpSpeed = 0.5f; // a large lerpSpeed makes the movement choppy
+
+            Vector3 nextPosition2 = Vector3.Lerp(transform.position, nextPosition, Time.deltaTime * lerpSpeed);
+
+            Debug.Log(string.Format("Update in ObjToSpawnScript client nextPosition {0} {1} {2}", nextPosition.x, nextPosition.y, nextPosition.z));
+
+            // this forceful update of the position is synced between the server and client
+            // other player can see the ball move in a line
+            Rigidbody rb = objToSpawn.GetComponent<Rigidbody>();
+
+            // transform.position = nextPosition;
+            // transform.position = nextPosition2;
+
+//            rb.Move(nextPosition, Quaternion.identity);
+            rb.Move(nextPosition2, Quaternion.identity);
+
+            //characterController.Move(nextPosition * Time.deltaTime);
+            //MoveObject2(transform, nextPosition);
+            // MoveObject(objToSpawn, nextPosition);
+        }
+
+    }
+
+    [ServerRpc]
+    public void MoveObject(GameObject obj, Vector3 nextPosition) // , Transform player, PlayerSpawnObject script)
+    {
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.Move(nextPosition, Quaternion.identity);
+        //GameObject spawned = Instantiate(obj, player.position + player.forward, Quaternion.identity);
+        //spawned.GetComponent<Rigidbody>().velocity = playerCamera.transform.forward * bulletSpeed;
+
+        //ServerManager.Spawn(spawned);
+        //SetSpawnedObject(spawned, script);
+    }
+
+    [ServerRpc]
+    public void MoveObject2(Transform t, Vector3 nextPosition) // , Transform player, PlayerSpawnObject script)
+    {
+        t.position = nextPosition;
+        //Rigidbody rb = obj.GetComponent<Rigidbody>();
         //rb.Move(nextPosition, Quaternion.identity);
+        //GameObject spawned = Instantiate(obj, player.position + player.forward, Quaternion.identity);
+        //spawned.GetComponent<Rigidbody>().velocity = playerCamera.transform.forward * bulletSpeed;
+
+        //ServerManager.Spawn(spawned);
+        //SetSpawnedObject(spawned, script);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(string.Format("ball collides to {0}", other.gameObject.name));
+        //Destroy(gameObject);
 
     }
 }
--- Assets/FishNet/zScripts/PlayerSpawnObject.cs
+++ Assets/FishNet/zScripts/PlayerSpawnObject.cs
@@ -8,29 +8,34 @@ public class PlayerSpawnObject : NetworkBehaviour
     public GameObject objToSpawn;
     [HideInInspector] public GameObject spwanedObject;
     public float bulletSpeed = 10f;
+    [SerializeField] Camera playerCamera;
+    [SerializeField] Vector3 shootDirection;
+    [SerializeField] Vector3 cameraForward;
+//    public ShootingInfo shootinfo;
 
     public override void OnStartClient()
     {
         base.OnStartClient();
         if (!base.IsOwner)
             GetComponent<PlayerSpawnObject>().enabled = false;
+
+        
+
     }
 
     private void Update()
     {
-        if(spwanedObject == null && Input.GetKeyDown(KeyCode.Alpha1))
+        if(spwanedObject == null && Input.GetMouseButtonDown(0))  // Input.GetKeyDown(KeyCode.Alpha1)) //Input.GetMouseButtonDown(0))
         {
-            if (objToSpawn == null)
-            {
-                Debug.Log("objToSpawn is null");
-            }else
-            {
-                Debug.Log("objToSpawn is not null");
-            }
 
-            SpawnObject(objToSpawn, transform, this);
+            cameraForward = Camera.main.transform.forward.normalized;
+            shootDirection = cameraForward;
+            Debug.Log(string.Format("1.7 shoot direction {0} {1} {2}", shootDirection.x, shootDirection.y, shootDirection.z));
+//            shootinfo.direction = shootDirection;
+//            shootinfo.UpdateScore();
+            SpawnObject(objToSpawn, transform, this, shootDirection);
         }
-        if (spwanedObject != null && Input.GetKeyDown(KeyCode.Alpha2))
+        if (spwanedObject != null && Input.GetMouseButtonDown(1)) // Input.GetKeyDown(KeyCode.Alpha2)) //Input.GetMouseButtonDown(1))
         {
             DespawnObject(spwanedObject);
         }
@@ -38,12 +43,26 @@ public class PlayerSpawnObject : NetworkBehaviour
     }
 
     [ServerRpc]
-    public void SpawnObject(GameObject obj, Transform player, PlayerSpawnObject script)
+    public void SpawnObject(GameObject obj, Transform player, PlayerSpawnObject script, Vector3 shootDirection)
     {
-        GameObject spawned = Instantiate(obj, player.position + player.forward, Quaternion.identity);
-        spawned.GetComponent<Rigidbody>().velocity = player.forward * bulletSpeed;
-        
-        ServerManager.Spawn(spawned);
+        Vector3 ballPosition = player.position + player.forward;
+
+        GameObject spawned = Instantiate(obj, ballPosition, Quaternion.identity);
+        //spawned.GetComponent<Rigidbody>().velocity = playerCamera.transform.forward * bulletSpeed;
+
+        Debug.Log(string.Format("1 serverRPC: location for the ball is {0} {1} {2}", ballPosition.x, ballPosition.y, ballPosition.z));
+
+        ObjToSpawnScript objscript = spawned.GetComponent<ObjToSpawnScript>();
+        objscript.moveDirection = shootDirection;
+        objscript.nextPosition = ballPosition; // playerCamera.transform.forward;
+        Debug.Log(string.Format("1.1 serverRPC: objscript moveDirection for the ball is {0} {1} {2}", objscript.moveDirection.x, objscript.moveDirection.y, objscript.moveDirection.z));
+
+ 
+
+        Debug.Log(string.Format("4 serverRPC: initial move dir for the ball is {0} {1} {2}", shootDirection.x, shootDirection.y, shootDirection.z));
+
+
+        ServerManager.Spawn(spawned, base.Owner);
         SetSpawnedObject(spawned, script);
     }
 
